42e52f44facc83d9f28ee7cac27a9650954793c9
Piyush Hinduja <74929365+piyushinduja@users.noreply.github.com>
2024-11-05 22:18:11 -0700

    Create profile.py

---
+++ profile.py
@@ -0,0 +1,19 @@
+import geni.portal as portal
+import geni.rspec.pg as pg
+
+# Create a portal context and define parameters
+pc = portal.Context()
+pc.defineParameter("nodeCount", "Number of Nodes", portal.ParameterType.INTEGER, 1)
+params = pc.bindParameters()
+
+# Create a Request object
+request = pc.makeRequestRSpec()
+
+# Create nodes and set disk image
+for i in range(params.nodeCount):
+    node = request.RawPC("node" + str(i))
+    node.disk_image = "urn:publicid:IDN+wisc.cloudlab.us+image+distribml-PG0:small-lan.node0"
+    node.addService(pg.Execute(shell="sh", command="/local/repository/setup.sh"))
+
+# Output the request RSpec
+pc.printRequestRSpec(request)
