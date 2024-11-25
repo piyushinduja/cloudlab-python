def4a17130a94602f278e568359c6ce3672231b4
Piyush Hinduja <74929365+piyushinduja@users.noreply.github.com>
2024-11-25 11:39:47 -0700

    Update profile.py

--- profile.py
+++ profile.py
@@ -13,10 +13,14 @@ request = pc.makeRequestRSpec()
 for i in range(params.nodeCount):
     node = request.RawPC("node" + str(i))
     node.disk_image = "urn:publicid:IDN+wisc.cloudlab.us+image+distribml-PG0:small-lan.node0"
-    node.addService(pg.Execute(shell='sh', command="echo {} > /local/node_rank".format(i)))
-    node.addService(pg.Execute(shell='sh', command="echo {} > /local/node_count".format(params.nodeCount)))
+    node.addService(pg.Execute(shell='sh', command="export MASTER_ADDR=10.10.1.1"))
+    node.addService(pg.Execute(shell='sh', command="export MASTER_PORT=29500"))
+    node.addService(pg.Execute(shell='sh', command="export WORLD_SIZE={}".format(params.nodeCount)))
+    node.addService(pg.Execute(shell='sh', command="export RANK={}".format(i)))
+    # node.addService(pg.Execute(shell='sh', command="echo {} > /local/node_rank".format(i)))
+    # node.addService(pg.Execute(shell='sh', command="echo {} > /local/node_count".format(params.nodeCount)))
     # node.addService(pg.Execute(shell='sh', command="chmod +x /local/repository/setup.sh"))
-    node.addService(pg.Execute(shell="sh", command="source /local/repository/setup.sh"))
+    # node.addService(pg.Execute(shell="sh", command="source /local/repository/setup.sh"))
 
 # Output the request RSpec
 pc.printRequestRSpec(request)
